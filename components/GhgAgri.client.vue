4bc4361e3af47c8c4a38a9737e9d26a4562c553f
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-02-23 12:41:24 +1300

    add ghgAgri graph

---
+++ components/GhgAgri.client.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <h1>GHG Emissions in Agriculture 2002</h1>
+    <nuxt-plotly
+      :data="pieChart.data"
+      :layout="pieChart.layout"
+      :config="pieChart.config"
+      style="width: 100%; height: 100%;"
+    ></nuxt-plotly>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+
+const GHG_Emissions_Agri_2002 = {
+  Fertiliser: 2465.32,
+  Sheep: 48984.06,
+  Pigs: 1.91,
+  BeefCattle: 30068.04,
+  DairyCattle: 30305.88,
+  Deer: 25603.23,
+};
+
+const labels = Object.keys(GHG_Emissions_Agri_2002);
+const values = Object.values(GHG_Emissions_Agri_2002);
+
+const colors = ['#F0E442', '#56B4E9', '#1A80BA', '#009E73', '#FFB44F', '#CC79A7'];
+
+const data = [
+  {
+    domain: { x: [0, 1], y: [0, 1] },
+    labels: labels,
+    values: values,
+    type: "pie",
+    hole: 0.7,
+    rotation: -30,
+    textinfo: "percent",
+    textposition: "outside",
+    insidetextorientation: "auto",
+    hoverinfo: "label+percent",
+    hovertemplate: "<b>%{label}</b><br>%{percent:.0%}<br>Total: %{value}<extra></extra>",
+    marker: { colors: colors },
+    textfont: { size: 18, family: "Overused Grotesk, sans-serif", color: "#898989" },
+  },
+];
+
+const layout = {
+  title: "GHG Emissions in Agriculture 2002",
+  autosize: true,
+  margin: { l: 0, r: 0, t: 0, b: 0 },
+  legend: {
+    x: 0.5,
+    y: -0.1,
+    xanchor: "center",
+    orientation: "h",
+    font: { size: 12 },
+  },
+  plot_bgcolor: "rgba(0,0,0,0)",
+  paper_bgcolor: "#F5F5F5",
+};
+
+const config = { responsive: true };
+
+const pieChart = reactive({
+  data: data,
+  layout: layout,
+  config: config,
+});
+</script>
