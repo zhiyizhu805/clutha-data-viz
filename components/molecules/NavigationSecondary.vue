41c61f3e3958080f32187be782ed240266df28ff
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-02-24 12:38:08 +1300

    add NavigatyionSecondary molecule

---
+++ components/molecules/NavigationSecondary.vue
@@ -0,0 +1,8 @@
+<!-- Navigation_secondary: the six indicators' navigation list -->
+<template>
+  <div
+    class="flex flex-row gap-0.5 justify-center items-center md:items-start items-1/4 md:bg-white z-10 md:mr-[3%] px-1 md:w-[46%] h-full"
+  >
+    <slot></slot>
+  </div>
+</template>
