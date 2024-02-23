acc75cb0f429658b900c19cce0f2f29c7803b7f4
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-02-24 09:59:47 +1300

    add IconFrameWithCircleBadge molecule

---
+++ components/molecules/IconFrameWithCircleBadge.vue
@@ -0,0 +1,24 @@
+<!-- a fixed width and height container used for the icon of navigation_tertiary_item -->
+  <template>
+  <div
+     :class="image_class1,image_class2" class="lg:p-2 p-1 xl:w-12 xl:h-12 lg:w-11 lg:h-11 md:w-9 md:h-9 w-7 h-7 bg-light-grey rounded-full flex justify-center items-center
+  "
+  >
+    <slot></slot>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    image_class1: {
+      type: String,
+      default: "",
+    },
+    image_class2: {
+      type: String,
+      default: "",
+    },
+  }}
+</script>
