12f7ca11c09e0bd912b29cb14d8750869e931804
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-02-24 10:28:13 +1300

    add unorderListNodiscStyle molecule

---
+++ components/molecules/UnoderlistNodiscStyle.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul :class="gap" class="w-full flex flex-col list-none">
+    <slot/>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    gap: {
+      type: String,
+      required: false,
+      default: "gap-0",
+    },
+  },
+};
+</script>
