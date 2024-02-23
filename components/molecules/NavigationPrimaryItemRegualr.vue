3d3d2fa115e0fe4b1ac7e48d5b3680cf3c50e3c7
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-02-24 11:37:51 +1300

    add NavigationPrimaryItemRegualr molecule

---
+++ components/molecules/NavigationPrimaryItemRegualr.vue
@@ -0,0 +1,30 @@
+<template>
+  <li
+    class="md:flex md:flex-grow md:rounded-full md:justify-center md:items-center group-hover:shadow-custom-border-xl md:w-full md:basis-1/3 navigation-list-nested-hover"
+    :class="[
+      $route.path === url
+        ? 'navigation-list-nested-active md:bg-light-grey'
+        : 'md:bg-light-grey',
+    ]"
+  >
+    <a
+      :href="url"
+      class="block md:p-0 text-gray-900 hover:bg-gray-100 md:hover:bg-none md:w-full md:h-full md:flex md:justify-center md:items-center"
+    >
+      <atomsNavTextstyle :url="url">
+        <slot></slot>
+      </atomsNavTextstyle>
+    </a>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
