3296456f0583df63ed268db010797d77c43d9775
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-02-24 10:26:47 +1300

    add reference molecule

---
+++ components/molecules/Reference.vue
@@ -0,0 +1,17 @@
+<template>
+  <moleculesUnoderlistNodiscStyle>
+    <slot></slot>
+  </moleculesUnoderlistNodiscStyle>
+</template>
+
+<script>
+export default {
+  props: {
+    gap: {
+      type: String,
+      required: false,
+      default: "gap-3",
+    },
+  },
+};
+</script>
