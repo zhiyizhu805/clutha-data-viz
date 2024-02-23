1daf00027e30b8ce7f9c7d969cae4443f06c909e
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-02-24 10:26:59 +1300

    add referenceItem molecule

---
+++ components/molecules/ReferenceItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <atomsBodyTextstyle>
+      <slot></slot>
+      <atomsLinkPlain :url="url">
+        {{ name }}
+      </atomsLinkPlain>
+      {{ additional_info }}
+    </atomsBodyTextstyle>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    additional_info: {
+      type: String,
+      required: false,
+      default: "",
+    },
+  },
+};
+</script>
