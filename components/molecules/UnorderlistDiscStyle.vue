c53a87db8415a2dfd9e7f53aec15b629cc491ad9
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-02-24 10:43:41 +1300

    Add UnorderlistDiscStyle molecule

---
+++ components/molecules/UnorderlistDiscStyle.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul :class="gap" class="w-full flex flex-col list-disc pt-5 pl-5">
+    <!-- call unorderlist_item atom here -->
+    <slot/>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    gap: {
+      type: String,
+      required: false,
+      default: "gap-0",
+    },
+  },
+};
+</script>
