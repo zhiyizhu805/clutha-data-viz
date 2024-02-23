4cada124d271bf574080c6dda9856a96aab05dde
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-02-24 11:37:34 +1300

    add NavigationPrimaryItemDropdownMenuItem molecule

---
+++ components/molecules/NavigationPrimaryItemDropdownMenuItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <li
+  class="md:flex md:flex-grow rounded-lg md:justify-center md:items-center group-hover:shadow-custom-border-xl md:w-full md:basis-1/3 navigation-list-nested-hover md:bg-light-grey"
+  :class="{ 'navigation-list-nested-active': $route.path === url }"
+  >
+    <a
+      :href="url"
+      class="block md:p-1 text-gray-900 hover:bg-gray-100 md:hover:bg-none md:w-full md:h-full md:flex md:justify-center md:items-center"
+    >
+      <atomsNavTextstyle :url="url">
+        <slot></slot>
+      </atomsNavTextstyle>
+    </a>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
