97b1473db7ce417eab1c6d157f200fd840572a36
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-02-24 16:52:42 +1300

    add NavigationTertiaryItem molecule

---
+++ components/molecules/NavigationTertiaryItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <li class="flex flex-row items-center justify-start gap-2">
+    <p>{{ navigation_title }}</p>
+    <moleculesIconFrameWithCircleBadge :image_class1="navigation_class" image_class2="environment_icon">
+      <slot></slot>
+    </moleculesIconFrameWithCircleBadge>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    navigation_title: {
+      type: String,
+      required: true,
+    },
+    navigation_class: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
