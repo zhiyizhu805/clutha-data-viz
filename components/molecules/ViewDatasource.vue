9e6d3c53322a1cdfb8d266ff51af4505180a0de9
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-02-24 17:23:16 +1300

    add viewDataSource molecule

---
+++ components/molecules/ViewDatasource.vue
@@ -0,0 +1,41 @@
+<!-- drawer init and toggle -->
+<template>
+  <button
+    class="flex flex-row gap-4 items-center justify-start"
+    type="button"
+    :data-drawer-target="flyout_id"
+    :data-drawer-show="flyout_id"
+    data-drawer-backdrop="false"
+    :aria-controls="flyout_id"
+    data-drawer-body-scrolling="true"
+  >
+    <moleculesIconFrame>
+      <img
+        src="~/assets/images/icons/icon_data_source.svg"
+        alt="Data source"
+        title="Data source"
+      />
+    </moleculesIconFrame>
+    <moleculesFooterTextstyle>
+      &lt;
+    </moleculesFooterTextstyle>
+    <moleculesFooter02Textstyle>
+      view data source
+    </moleculesFooter02Textstyle>
+  </button>
+
+  <!-- placeholder -->
+  <slot></slot>
+</template>
+
+<script>
+export default {
+  props: {
+    flyout_id: {
+      type: String,
+      required: true,
+      default: "drawer-disabled-backdrop",
+    },
+  },
+};
+</script>
