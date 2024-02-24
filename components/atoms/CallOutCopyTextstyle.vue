73d23bf136579471e921a07038e8b36d97d4c737
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-02-24 17:16:47 +1300

    bug fixed: add slot tag for calloutcopytextstle atom

--- components/atoms/CallOutCopyTextstyle.vue
+++ components/atoms/CallOutCopyTextstyle.vue
@@ -2,6 +2,6 @@
   <p
     class="text-black font-regular font-overused-grotesk xl:text-2xl xl:leading-9 lg:text-xl lg:leading-7 md:text-base md:leading-tight text-xl leading-relaxed tracking-tighter"
   >
-    <slot/>
+    <slot></slot>
   </p>
 </template>
