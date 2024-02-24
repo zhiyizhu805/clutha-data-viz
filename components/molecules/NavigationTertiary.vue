ed5b9963b3a86503d8895e6ace35d17f92db28ce
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-02-24 16:52:26 +1300

    add NavigationTertiary molecule

---
+++ components/molecules/NavigationTertiary.vue
@@ -0,0 +1,8 @@
+<!-- Navigation_tertiary: the homepage navigation list -->
+<template>
+  <ul
+    class="text-black uppercase font-bold font-overused-grotesk xl:text-2xl xl:leading-snug lg:text-lg lg:leading-tight md:text-sm md:leading-3 text-sm leading-none tracking-tight md:p-10 p-7 border-4 border-grey-900 rounded-3xl grid md:grid-cols-2 grid-cols-1 md:gap-6 gap-4"
+  >
+    <slot></slot>
+  </ul>
+</template>
