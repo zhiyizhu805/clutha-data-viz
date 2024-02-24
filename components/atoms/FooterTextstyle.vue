6bd92cbe25f14b2606faeda0c5657a0ace639a23
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-02-24 17:16:20 +1300

    add dynamic class for footertextstle atom

--- components/atoms/FooterTextstyle.vue
+++ components/atoms/FooterTextstyle.vue
@@ -1,7 +1,18 @@
 <template>
   <p
-    class="font-inter font-medium xl:text-sm xl:leading-relaxed text-xs lg:leading-tight md:leading-none leading-3 tracking-tight"
+    :class="text_color" class="font-inter font-medium xl:text-sm xl:leading-relaxed text-xs lg:leading-tight md:leading-none leading-3 tracking-tight"
   >
-    <slot/>
+    <slot></slot>
   </p>
 </template>
+
+<script>
+export default {
+  props: {
+    text_color: {
+      type: String,
+      default: "text-black",
+    },
+  },
+};
+</script>
