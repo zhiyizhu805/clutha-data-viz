dcb0dd35495fce176e8c5f899fd4976b7303d0a5
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-02-24 16:57:51 +1300

    add iconFrame molecule

---
+++ components/molecules/IconFrame.vue
@@ -0,0 +1,7 @@
+<!-- a fixed width and height container used for the icon of navigation_secondary_item -->
+<template>
+  <div class="xl:w-11 xl:h-11 lg:w-10 lg:h-10 md:w-8 md:h-8 w-6 h-6">
+    <slot></slot>
+  </div>
+</template>
+
