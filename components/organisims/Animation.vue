ea8a9946e1b4883589d0872879d64a44066d3dfe
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-02-25 16:44:47 +1300

    add animation  organisim

---
+++ components/organisims/Animation.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- animation section -->
+  <div class="relative">
+    <img
+      class="w-full mb-12 environment-graph"
+      src="/assets/images/graphs/hero.svg"
+      alt="Environmental illustration"
+      title="Environmental illustration"
+      width="56"
+      height="56"
+    />
+    <moleculesNavigationTertiary>
+      <slot></slot>
+    </moleculesNavigationTertiary>
+  </div>
+</template>
