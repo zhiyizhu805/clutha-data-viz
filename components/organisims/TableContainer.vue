6d06d64e89fb6415e0488c9a81e783a677df62c7
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-02-25 16:47:21 +1300

    add tablecontainer organisim

---
+++ components/organisims/TableContainer.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="overflow-x-auto">
+    <table
+      class="min-w-full bg-grey-900 text-white text-body font-light font-inter tracking-default xl:text-lg xl:leading-7 lg:text-base lg:leading-snug md:text-xs md:leading-none text-xs leading-tight"
+    >
+      <slot></slot>
+    </table>
+  </div>
+</template>
