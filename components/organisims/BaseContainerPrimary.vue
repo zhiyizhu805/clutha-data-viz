686433a362758e3f2782eb1a18a104e4f686907f
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-02-27 11:58:20 +1300

    add basecontainer secondary

---
+++ components/organisims/BaseContainerPrimary.vue
@@ -0,0 +1,8 @@
+<!-- set the page gutters -->
+<template>
+  <div
+    class="container mx-auto h-auto xl:px-24 lg:px-16 md:px-11"
+  >
+    <slot></slot>
+  </div>
+</template>
---
+++ components/organisims/BaseContainerSecondary.vue
@@ -0,0 +1,8 @@
+<!-- set the vertical gaps -->
+<template>
+  <div
+    class="flex flex-col xl:gap-24 lg:gap-16 md:gap-11 gap-10"
+  >
+    <slot></slot>
+  </div>
+</template>
