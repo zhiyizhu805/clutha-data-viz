0143595f5aa2de1c70ae091b5ed5875db50ac3de
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-02-27 14:32:20 +1300

    add BaseArticleContainerSecondary organisim

---
+++ components/organisims/BaseArticleContainerSecondary.vue
@@ -0,0 +1,12 @@
+<!-- BaseArticleContainerPrimarySecondary.vue is a reusable component featuring a stacked layout. 
+     The first layer consists of content aligned to the right with a width of 60%, while the second layer encompasses a full-width graph.  -->
+<template>
+  <div
+    class="w-full flex flex-col items-end justify-start basis-3/5 xl:gap-10 lg:gap-8 gap-6"
+  >
+    <organisimsChildContainerSixtyPercentWidth>
+      <slot name="content-section"></slot>
+    </organisimsChildContainerSixtyPercentWidth>
+    <slot></slot>
+  </div>
+</template>
