1b4302d250c32fa5d4d0440244b72a6faef6f77d
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-02-28 15:32:39 +1300

    bug fixed: remove duplicate element tags

--- components/molecules/NavigationPrimaryItemDropdownMenu.vue
+++ components/molecules/NavigationPrimaryItemDropdownMenu.vue
@@ -5,12 +5,9 @@
     <button
       id="dropdownNavbarLink"
       data-dropdown-toggle="dropdownNavbar"
-      class="flex items-center justify-between w-full text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 
-     md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+      class="flex items-center justify-between w-full text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
     >
-      <atomsNavTextstyle>
-        About
-      </atomsNavTextstyle>
+      <atomsNavTextstyle> About </atomsNavTextstyle>
       <svg
         class="w-2.5 h-2.5 ms-2.5"
         aria-hidden="true"
@@ -37,11 +34,7 @@
       class="py-3 text-sm text-grey-900 dark:text-gray-200"
       aria-labelledby="dropdownLargeButton"
     >
-      <li>
-        <atomsNavTextstyle>
-          <slot></slot>
-        </atomsNavTextstyle>
-      </li>
+        <slot></slot>
     </ul>
   </div>
 </template>
