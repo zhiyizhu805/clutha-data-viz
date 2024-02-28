384326e728242a27d1043a1799e25fddff3bf287
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-02-28 23:26:11 +1300

    hide arrow svg when hover naviagtion secondary item

--- components/molecules/NavigationSecondaryItem.vue
+++ components/molecules/NavigationSecondaryItem.vue
@@ -2,7 +2,7 @@
 <template>
 <nuxt-link
   :to="url"
-  class="group md:w-full md:flex-1 md:flex-grow md:rounded-t-none md:rounded-b-full md:items-center w-10 h-10 rounded-full flex md:flex-col justify-center z-20 items-center cursor-pointer md:gap-2 bg-light-grey hover:bg-light-grey hover:xl:shadow-custom-border-xl hover:md:shadow-custom-border-md hover:shadow-custom-border-sm hover:md:h-full hover:transition-all hover:duration-500 hover:ease-out"
+  class="group md:w-full md:flex-1 md:flex-grow md:rounded-t-none md:rounded-b-full md:items-center w-10 h-10 rounded-full flex md:flex-col justify-center z-20 items-center cursor-pointer md:gap-2 bg-light-grey hover:bg-light-grey hover:xl:shadow-custom-border-xl hover:md:shadow-custom-border-md hover:shadow-custom-border-sm hover:md:h-full hover:transition-all hover:duration-500 hover:ease-out hover:md:pt-[12%]"
   :class="{
     'md:h-full xl:shadow-custom-border-xl md:shadow-custom-border-md shadow-custom-border-sm md:pt-[12%]': active,
     'md:h-[82%] lg:rounded-b-full lg:items-center md:pt-[7%]': !active
@@ -14,7 +14,7 @@
     </moleculesIconFrame>
     <svg
       v-if="!active"
-      class="hidden md:block md:w-2.5 md:h-2.5"
+      class="hidden md:block md:w-2.5 md:h-2.5 group-hover:hidden"
       aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
