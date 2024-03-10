1c4677fff49b494a4aafd43630d97431c52b4b74
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-03-10 22:49:31 +1300

    add vision page content

--- pages/vision.vue
+++ pages/vision.vue
@@ -1,5 +1,31 @@
 <template>
   <organismsHeadline> Vision </organismsHeadline>
+  <organismsBaseArticleContainerQuaternary>
+  <atomsBodyTextstyle>
+    In envisioning a thriving and resilient future for the Upper Clutha
+    Catchment, our holistic catchment health framework embraces the profound
+    interconnection of people and place, recognising the intrinsic value of Te
+    Taiao. Grounded in community leadership and collaboration, we are committed
+    to fostering a collective sense of responsibility for the well-being of our
+    environment. Our vision extends beyond scientific data; it encompasses the
+    lived experiences, cultures, and aspirations of the communities within our
+    catchment.
+  </atomsBodyTextstyle>
+  <atomsBodyTextstyle>
+    This framework serves as a compass, guiding environmental restoration
+    activities and investment with a keen understanding of the delicate balance
+    between ecological health and human well-being. By weaving the wisdom of
+    local communities and the expertise of diverse organisations, we strive for
+    a sustainable and harmonious coexistence between nature and society for
+    present and future generations to come.
+  </atomsBodyTextstyle>
+  <atomsBodyTextstyle>
+    Join us on this journey as we strive to deepen our connection to Te Taiao
+    and pave the way towards a healthier, more sustainable future for the Upper
+    Clutha Catchment. Together, let&#39;s embark on a transformative endeavour
+    rooted in respect, reciprocity, and resilience.
+  </atomsBodyTextstyle>
+  </organismsBaseArticleContainerQuaternary>
 </template>
 
 <script setup lang="ts">
