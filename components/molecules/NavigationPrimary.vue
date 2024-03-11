a550e4048e8c4ccd6bbfe695de10bc363cf12295
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-03-12 12:10:10 +1300

    remove not used components

--- components/molecules/NavigationPrimary.vue
+++
@@ -1,8 +0,0 @@
-<!-- navigation_primary should contain navigation_primary_item. The list is horizontal in screen larger than md,vertical(hidden in a dropdown container) in xs screen -->
-<template>
-  <ul
-    class="flex flex-col p-4 mt-4 font-medium border bg-light-grey rounded-lg md:justify-around rtl:space-x-reverse md:w-full md:h-full md:flex-row md:mt-0 md:p-0 md:border-0 md:bg-white md:gap-0.5"
-  >
-    <slot></slot>
-  </ul>
-</template>
--- components/organisms/BaseContainerPrimary.vue
+++
@@ -1,6 +0,0 @@
-<!-- set horizonral page gutters -->
-<template>
-  <div class="px-0.5 md:px-3">
-    <slot></slot>
-  </div>
-</template>
